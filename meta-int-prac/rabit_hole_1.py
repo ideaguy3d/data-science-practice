ddc29b843034c1f39954717243eafa9ab56d0bbb
Cal Geek <javascript.uiux@gmail.com>
2025-09-16 19:50:14 -0700

    Update rabit_hole_1.py

--- meta-int-prac/rabit_hole_1.py
+++ meta-int-prac/rabit_hole_1.py
@@ -1,55 +1,55 @@
 from typing import List
 
-def getMaxVisitableWebpages(N: int, L: List[int]) -> int:
-    state = [0] * (N+1) # 0=unvisited 1=visiting 2=done
+def getMaxVisitableWebpages(N: int, L: List[int]) -> int:  
+    ans = 0 
+    state = [0] * (N+1)
     dp = [0] * (N+1)
-    ans = 0
 
-    for start in range(1, N + 1):
-        print(start, ' ', L[start-1]) 
-
-        stack = []  
+    for start in range(1, N+1):
+        if state[start] != 0:
+            continue
+        
         u = start 
+        stack = []
 
-        while True: 
+        while True:
             if state[u] == 0:
                 state[u] = 1
                 stack.append(u)
                 u = L[u-1]
                 continue 
+                     
             
             elif state[u] == 1:
                 # cycle 
                 cycle = []
                 while True:
-                    v = stack.pop()
+                    v = stack.pop() 
                     cycle.append(v)
-                    if u == v:
+                    if v == u:
                         break 
                 cycle_len = len(cycle)
                 for v in cycle:
-                    dp[v] = cycle_len 
+                    dp[v] = cycle_len
                     state[v] = 2
-                
-                # back propagate 
+
+                # back prop
                 while stack:
                     v = stack.pop()
                     dp[v] = 1 + dp[L[v-1]]
-                    state[v] = 2
-                
+                    state[v] = 2 
                 break 
-
+            
             elif state[u] == 2:
                 while stack:
                     v = stack.pop()
                     dp[v] = 1 + dp[L[v-1]]
                     state[v] = 2
-
-                break  
-            
-        ans = max(ans, dp[start])
-
-    return ans 
+                break    
+        
+        ans = max(ans, dp[start]) 
+    print(f"\nanswer = ${ans}\n")
+    return ans  
 
 print('\n----\n')
 print(getMaxVisitableWebpages(6, [2, 3, 4, 2, 6, 5]))
