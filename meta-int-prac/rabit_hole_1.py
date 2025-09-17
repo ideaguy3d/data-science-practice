af3b87f68a10be208d2332493b6e5401a8e81102
Cal Geek <javascript.uiux@gmail.com>
2025-09-17 09:28:07 -0700

    Update rabit_hole_1.py

--- meta-int-prac/rabit_hole_1.py
+++ meta-int-prac/rabit_hole_1.py
@@ -7,8 +7,8 @@ def getMaxVisitableWebpages(N: int, L: List[int]) -> int:
 
     for start in range(1, N+1):
         if state[start] != 0:
-            continue
-        
+            continue 
+
         u = start 
         stack = []
 
@@ -17,41 +17,44 @@ def getMaxVisitableWebpages(N: int, L: List[int]) -> int:
                 state[u] = 1
                 stack.append(u)
                 u = L[u-1]
-                continue 
-                     
-            
+                continue  
+
             elif state[u] == 1:
                 # cycle 
                 cycle = []
                 while True:
                     v = stack.pop() 
                     cycle.append(v)
-                    if v == u:
-                        break 
+                    if u == v:
+                        break  
                 cycle_len = len(cycle)
                 for v in cycle:
                     dp[v] = cycle_len
-                    state[v] = 2
-
+                    state[v] = 2 
+                
                 # back prop
                 while stack:
                     v = stack.pop()
                     dp[v] = 1 + dp[L[v-1]]
-                    state[v] = 2 
+                    state[v] = 2
                 break 
-            
+
             elif state[u] == 2:
                 while stack:
                     v = stack.pop()
                     dp[v] = 1 + dp[L[v-1]]
                     state[v] = 2
-                break    
-        
+                break 
+
         ans = max(ans, dp[start]) 
-    print(f"\nanswer = ${ans}\n")
+    
     return ans  
 
+# --- Sample Tests ---
 print('\n----\n')
+print(getMaxVisitableWebpages(4, [4, 1, 2, 1]))    # 4
+print(getMaxVisitableWebpages(5, [4, 3, 5, 1, 2])) # 3
+print(getMaxVisitableWebpages(5, [2, 4, 2, 2, 3])) # 4
 print(getMaxVisitableWebpages(6, [2, 3, 4, 2, 6, 5]))
 print('\n----\n')
 
