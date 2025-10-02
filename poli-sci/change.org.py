c78c6ba6ab7015b8f75bdac0d4e3cfb90cc76396
Cal Geek <javascript.uiux@gmail.com>
2025-10-02 12:42:26 -0700

    poli sci lessons

--- poli-sci/change.org.py
+++ poli-sci/change.org.py
@@ -4,7 +4,7 @@ interesting petitions I found on change.org
 
 interesting_petitions = [
 
-    "https://chng.it/g8thmYgmdD"
+    "https://chng.it/g8thmYgmdD",
     "https://www.change.org/p/joseph-r-biden-restore-the-white-house-s-we-the-people-petition-site?recruiter=1387247085&recruited_by_id=5b55e660-895f-11f0-bf79-a949e37bc6df&utm_source=share_petition&utm_campaign=psf_combo_share_initial&utm_term=psf&utm_medium=copylink&utm_content=cl_sharecopy_27399102_en-US%3A7"
 
 ]
