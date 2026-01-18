28eff96a2f77d7f65b6e51a14db14853c4debdfa
Cal Geek <javascript.uiux@gmail.com>
2026-01-18 07:21:00 -0800

    more dfs prac

--- meta-int-prac/graph_practice.js
+++ meta-int-prac/graph_practice.js
@@ -79,12 +79,6 @@ class DirectedGraph {
         this.edges[originVertex][destVertex] = weight;
     }
     
-    removeEdge(originVertex, destVertex) {
-        if (this.edges[originVertex] && this.edges[originVertex][destVertex] != undefined) {
-            delete this.edges[originVertex][destVertex];
-        }
-    }
-    
     removeVertex(vertex) {
         for (var adjacentVertex in this.edges[vertex]) {
             this.removeEdge(adjacentVertex, vertex);
@@ -92,8 +86,14 @@ class DirectedGraph {
 
         delete this.edges[vertex];
     }
+
+    removeEdge(originVertex, destVertex) {
+        if (this.edges[originVertex] && this.edges[originVertex][destVertex] != undefined) {
+            delete this.edges[originVertex][destVertex];
+        }
+    }
     
-    bfs(vertex) {
+    bfs_1(vertex) {
         var queue = [], visited = {};
 
         queue.push(vertex);
@@ -109,13 +109,30 @@ class DirectedGraph {
             }
         }
     }
+
+    bfs(vertex) {
+        var queue = [], visited = {}; 
+
+        queue.push(vertex); 
+
+        while (queue.length) {
+            vertex = queue.shift();
+            if (!visited[vertex]) {
+                visited[vertex] = true; 
+                logs.push(vertex); 
+                for (var adjacent_vertex in this.edges[vertex]) {
+                    queue.push(adjacent_vertex); 
+                }
+            }
+        }
+    }
     
-    dfs(vertex) {
+    dfs_1(vertex) {
         var visited = {};
         this._dfs(vertex, visited);
     }
 
-    _dfs(vertex, visited) {
+    _dfs_1(vertex, visited) {
         visited[vertex] = true;
         logs.push(vertex);
         for (var adjacentVertex in this.edges[vertex]) {
@@ -124,31 +141,76 @@ class DirectedGraph {
             }
         }
     }
+
+    dfs(vertex) {
+        var visited = {};
+        this._dfs(vertex, visited);
+    }
+
+    _dfs(vertex, visited) {
+        visited[vertex] = true; 
+        logs.push(vertex); 
+        for(var adjacent_vertex in this.edges[vertex]) {
+            if (!visited[adjacent_vertex]) {
+                this._dfs(adjacent_vertex, visited); 
+            }
+        }
+    }
 }
 
-var digraph1 = new DirectedGraph();
-digraph1.addVertex("A");
-digraph1.addVertex("B");
-digraph1.addVertex("C");
-digraph1.addEdge("A", "B", 1);
-digraph1.addEdge("B", "C", 2);
-digraph1.addEdge("C", "A", 3);
-digraph1.addVertex("D");
-digraph1.addEdge("B", "D", 2)
+// var digraph1 = new DirectedGraph();
+// digraph1.addVertex("A");
+// digraph1.addVertex("B");
+// digraph1.addVertex("C");
+// digraph1.addEdge("A", "B", 1);
+// digraph1.addEdge("B", "C", 2);
+// digraph1.addEdge("C", "A", 3);
+// digraph1.addVertex("D");
+// digraph1.addEdge("B", "D", 2)
+// BREADTH
 // digraph1.bfs("B"); 
 
-var graph2 = new DirectedGraph();
-graph2.addVertex(1);
-graph2.addVertex(2);
-graph2.addEdge(1, 2, 1);
-graph2.addVertex(3);
-graph2.addVertex(4);
-graph2.addVertex(5);
-graph2.addEdge(2, 3, 8);
-graph2.addEdge(3, 4, 10);
-graph2.addEdge(4, 5, 100);
-graph2.addEdge(1, 5, 88);
-graph2.dfs(1)
+var digraph1b = new DirectedGraph();
+digraph1b.addVertex("A");
+digraph1b.addVertex("B");
+digraph1b.addVertex("C");
+digraph1b.addVertex("D");
+digraph1b.addVertex("E");
+digraph1b.addVertex("F");
+digraph1b.addVertex("G");
+digraph1b.addVertex("H");
+digraph1b.addVertex("i");
+digraph1b.addVertex("j");
+digraph1b.addVertex("k");
+digraph1b.addEdge("A", "B");
+digraph1b.addEdge("B", "C");
+digraph1b.addEdge("B", "E");
+digraph1b.addEdge("B", "j");
+digraph1b.addEdge("B", "k");
+digraph1b.addEdge("C", "D");
+digraph1b.addEdge("D", "G");
+digraph1b.addEdge("D", "F");
+digraph1b.addEdge("G", "H");
+digraph1b.addEdge("F", "i");
+// BREADTH
+// digraph1b.bfs("A"); 
+// DEPTH 
+digraph1b.dfs("A"); 
+
+
+// var digraph2 = new DirectedGraph();
+// digraph2.addVertex(1);
+// digraph2.addVertex(2);
+// digraph2.addEdge(1, 2, 1);
+// digraph2.addVertex(3);
+// digraph2.addVertex(4);
+// digraph2.addVertex(5);
+// digraph2.addEdge(2, 3, 8);
+// digraph2.addEdge(3, 4, 10);
+// digraph2.addEdge(4, 5, 100);
+// digraph2.addEdge(1, 5, 88);
+// DEPTH 
+// digraph2.dfs(1)
 
 
 
